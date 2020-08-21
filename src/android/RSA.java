7d7da25f9e92efa354388b53cfea573d91ecd2c9
kaveepart <35480359+kaveepart@users.noreply.github.com>
2020-08-21 11:32:37 +0700

    Update RSA.java
    remove setUserAuthenticationRequired  and setUserAuthenticationValidityDurationSeconds.

--- src/android/RSA.java
+++ src/android/RSA.java
@@ -45,8 +45,6 @@ public class RSA extends AbstractRSA {
                 .setCertificateNotBefore(Calendar.getInstance().getTime())
                 .setCertificateNotAfter(notAfter.getTime())
                 .setAlgorithmParameterSpec(new RSAKeyGenParameterSpec(2048, RSAKeyGenParameterSpec.F4))
-                .setUserAuthenticationRequired(true)
-                .setUserAuthenticationValidityDurationSeconds(userAuthenticationValidityDuration)
                 .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1)
                 .setBlockModes(KeyProperties.BLOCK_MODE_ECB)
                 .build();
